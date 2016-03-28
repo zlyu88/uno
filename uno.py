fa726e63359188b7c71c3cf10274e2e0470e4969
Napoleon-Black <my.napoleon@gmail.com>
2016-03-29 01:23:56 +0300

    More User-Friendly

--- uno.py
+++ uno.py
@@ -1,4 +1,5 @@
 from random import choice
+from time import sleep
 
 class Uno():
     deck = ['1_y', '2_y', '3_y', '4_y', '5_y', '6_y', '7_y', '8_y', '9_y', 'Pass_y', 'pass+2_y', 'wild', 'wild+4', '1_r', '2_r', '3_r', '4_r', '5_r', '6_r', '7_r', '8_r', '9_r', 'Pass_r', 'pass+2_r', 'wild', 'wild+4', '1_g', '2_g', '3_g', '4_g', '5_g', '6_g', '7_g', '8_g', '9_g', 'Pass_g', 'pass+2_g', 'wild', 'wild+4', '1_b', '2_b', '3_b', '4_b', '5_b', '6_b', '7_b', '8_b', '9_b', 'Pass_b', 'pass+2_b', 'wild', 'wild+4']
@@ -93,60 +94,62 @@ class Uno():
             card = choice(deck)
             deck.pop(deck.index(card))
             bot_cards.append(card)
-            print base
             self.bot_choice(bot_cards, base, summ2, count)
         elif count % 2 == 0:
             card = choice(deck)
             deck.pop(deck.index(card))
             player_cards.append(card)
-            print base
             self.player_choice(player_cards, base, summ1, count)
 
 
     def player_choice(self, player_cards, base, summ1, count):
-        print '-----Player move'
-        print 'BASE - ' + base
-        print player_cards
+        print '\n----- Player move -----\n'
+        print 'BASE is: %s\n'% base
+        print 'Your cards: %s'% player_cards
 
         move = []
         for card in player_cards:
             if card[0] == base[0] or base[len(base)-1] == card[len(card)-1] or card[0] == 'w':
                 move.append(card)
-        print move
+        print 'You can use: %s'% move
         if move:
-            output = move[int(raw_input('Player put ' )) - 1]
+            output = move[int(raw_input('Your choice:  ' )) - 1]
             player_cards.pop(player_cards.index(output))
             if player_cards:
                 self.action(count, base, output)
             else:
                 print 'Player win!'
         elif not move and self.deck and not summ1:
-            print 'Player takes 1 card' + ' BASE is ' + base
+            sleep(2)
+            print 'Player takes 1 card\nBASE is: %s'% base
             summ1 += 1
             self.no_card(count, player_cards, self.bot_cards, self.deck, base, summ1, self.summ2)
         elif summ1:
             print 'Player has no card to put'
-            print str(count) + 'before'
+            print str(count) + ' before'
             count += 1
-            print str(count) + 'after'
+            print str(count) + ' after'
             output = base
             self.bot_choice(self.bot_cards, base, self.summ2, count)
 
     def bot_choice(self, bot_cards, base, summ2, count):
-        print '-----Bot move'
-        print 'BASE - ' + base
-        print bot_cards
+        print '\n-----Bot move-----\n'
+        print 'BASE is: %s\n'% base
+        print 'Bot cards: %s'% bot_cards
+        sleep(2)
         move = []
         for card in bot_cards:
             if card[0] == base[0] or base[len(base)-1] == card[len(card)-1]:
                 move.append(card)
             elif card == 'wild' or card == 'wild+4' or card[0:4] == base[0:4]:
                 move.append(card)
-        print move
+        print 'Bot can chose: %s'% move
+        sleep(2)
         if move:
             output = choice(move)
             bot_cards.pop(bot_cards.index(output))
-            print '---bot put ' + output
+            print 'Bot chose: %s '% output
+            sleep(1)
             if bot_cards:
                 self.action(count, base, output)
             else:
@@ -157,9 +160,9 @@ class Uno():
             self.no_card(count, self.player_cards, bot_cards, self.deck, base, self.summ1, summ2)
         elif summ2:
             print 'Bot has no card to put'
-            print str(count) + 'before'
+            print str(count) + ' before'
             count += 1
-            print str(count) + 'after'
+            print str(count) + ' after'
             output = base
             self.player_choice(self.player_cards, base, self.summ1, count)
             
